51b31fdc8218ab0f79fcbbf991fc52560fc37044
badir <badir.al-sayyed@stud-hshl.de>
2020-12-15 00:40:21 +0100

    Working Scene Changer

--- Assets/Scripts/SkyboxChanger.cs
+++ Assets/Scripts/SkyboxChanger.cs
@@ -1,43 +1,82 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 public class SkyboxChanger : MonoBehaviour
 {
     public Material[] skyBox;
+    public GameObject[] umgebung;
 
     private int i = 0;
-    // Start is called before the first frame update
+    private int j = 1;
+
+    private float timePassed = 0;
+    private bool pause = false;
+    public float maxTime;
+
     void Start()
     {
         RenderSettings.skybox = skyBox[0];
+        umgebung[0].SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (pause)
+        {
+            timePassed = timePassed + Time.deltaTime;
+            if (timePassed >= maxTime)
+            {
+                timePassed = 0;
+                pause = false;
+            }
+        }
     }
 
     public void SkyboxUp()
     {
-        if (i == skyBox.Length)
+        if (!pause)
         {
-            i = 0;
+            pause = true;
+            if (i == skyBox.Length)
+            {
+                i = 0;
+            }
+            i++;
+            RenderSettings.skybox = skyBox[i];
+            Debug.Log("Skybox nummer " + i); 
         }
-        i++;
-        RenderSettings.skybox = skyBox[i];
-        Debug.Log("Skybox nummer " + i);
     }
 
     public void SkyboxDown()
     {
-        if (i == skyBox.Length)
+        if (!pause)
         {
-            i = 0;
+            pause = true;
+            if (i == skyBox.Length)
+            {
+                i = 0;
+            }
+            i--;
+            RenderSettings.skybox = skyBox[i];
+            Debug.Log("Skybox nummer " + i);
         }
-        i--;
-        RenderSettings.skybox = skyBox[i];
-        Debug.Log("Skybox nummer " + i);
+    }
+
+    public void UmgebungUp()
+    {
+        if (!pause)
+        {
+            pause = true;
+            if (j == umgebung.Length)
+            {
+                j = 0;
+            }
+            umgebung[j].SetActive(false);
+            j++;
+            umgebung[j].SetActive(true);
+        }
     }
 }
