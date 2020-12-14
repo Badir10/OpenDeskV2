a795c06d6c7c5c75be44860335d1d6af3c8da52d
badir <badir.al-sayyed@stud-hshl.de>
2020-12-14 18:20:07 +0100

    Basic Skyboxchanger

---
+++ Assets/Scripts/SkyboxChanger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxChanger : MonoBehaviour
+{
+    public Material[] skyBox;
+
+    private int i = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RenderSettings.skybox = skyBox[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangingSkybox()
+    {
+        if (i == skyBox.Length)
+        {
+            i = 0;
+        }
+        i++;
+        RenderSettings.skybox = skyBox[i];
+        Debug.Log("Skybox nummer " + i);
+    }
+}
