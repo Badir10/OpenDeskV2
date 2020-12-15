055edd5f2447069f750e7336263e0db5ac7fffb9
Sebastian Schmidt <sebastian6240@web.de>
2020-12-15 02:14:20 +0100

    Key

--- Assets/Scripts/KeyBoard.cs
+++ Assets/Scripts/KeyBoard.cs
@@ -6,8 +6,28 @@ public class KeyBoard : MonoBehaviour
 {
     public char Key;
 
+    private float timePassed = 0;
+    private bool pause = false;
+    float maxTime = 0.2f;
+    private void Update()
+    {
+        if (pause)
+        {
+            timePassed = timePassed + Time.deltaTime;
+            if (timePassed >= maxTime)
+            {
+                timePassed = 0;
+                pause = false;
+            }
+        }
+    }
+
     public void PrintKey()
     {
-        ScreenController.PrintKey(Key);
+        if (!pause)
+        {
+            pause = true;
+            ScreenController.PrintKey(Key);
+        }
     }
 }
